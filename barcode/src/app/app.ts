473d2b996d470f367e34e577506aff6833bed49a
phhorsap <43028614+phhorsap@users.noreply.github.com>
2025-10-28 09:51:14 +0000

    barcode with index

--- barcode/src/app/app.ts
+++ barcode/src/app/app.ts
@@ -13,15 +13,21 @@ export class App  implements AfterViewInit{
 
   protected readonly title = signal('barcode');
  item = [1,2,3,4,5,6,7,8,9]
-  ngAfterViewInit() {
-    JsBarcode('#barcode', '123456789012', {
-      format: 'CODE128',
-      lineColor: '#000',
-      width: 2,
-      height: 100,
-      displayValue: true,
+
+ lieferscheine = ['LS1001', 'LS1002', 'LS1003', 'LS1004'];
+
+ 
+ngAfterViewInit() {
+    this.lieferscheine.forEach((code, index) => {
+      JsBarcode(`#barcode-${index}`, code, {
+        format: 'CODE128',
+        lineColor: '#000',
+        width: 2,
+        height: 100,
+        displayValue: true,
+      });
     });
   }
- 
+
 
 }
